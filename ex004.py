d8451cb69bf13901d880e38b4e5f1457df7264db
davidluiz91 <dvdluiz1991@gmail.com>
2022-07-12 09:35:01 -0300

    ex004

---
+++ ex004.py
@@ -0,0 +1,12 @@
+#Faça um Programa que leia algo pelo teclado e mostre na tela o seu
+#tipo primitivo e todas as informações possiveis sobre ele.
+
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um número? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('é alfanumérico? ', a.isalnum())
+print('Está em maiúscula? ', a.isupper())
+print('Está em minúscula? ', a.islower())
+print('Está capitalizada? ', a.istitle())
